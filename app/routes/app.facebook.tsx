12a6e51573b50d46540176914258f2db15600155
Bui Anh Do <anhdobui2002@gmail.com>
2025-12-11 21:27:08 +0700

    first commit

---
+++ app/routes/app.facebook.tsx
@@ -0,0 +1,15 @@
+import { TitleBar } from "@shopify/app-bridge-react";
+
+export default function FacebookPage() {
+  return (
+    <s-page>
+      <TitleBar title="Facebook page"></TitleBar>
+      <s-section heading="Multiple pages">
+        <s-paragraph>
+        hello Facebook
+          
+        </s-paragraph>
+      </s-section>
+    </s-page>
+  );
+}
